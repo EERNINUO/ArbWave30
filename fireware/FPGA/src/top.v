650fd060e209a6f80375044b7447ac752fe04648
eerninuo <eerninuo88@gmail.com>
2026-06-15 15:53:13 +0800

    feat：定义顶部接口

--- fireware/FPGA/src/top.v
+++ fireware/FPGA/src/top.v
@@ -10,3 +10,34 @@
  * ...
  */
 
+module ArbWave30 (
+    // system
+    input sys_clk,
+    input sys_rst_n,
+    output sys_clk_rst,
+
+    // MCU_Ctrl_interface 
+    input ctrl_spi_cs,
+    input ctrl_spi_sck,
+    input ctrl_spi_mosi,
+    output ctrl_spi_miso,
+    input ctrl_gpio1,
+    input ctrl_gpio2,
+
+    // DAC_interface
+    input dco,  
+    output reg[15:0] data_ch1,
+    output reg[15:0] data_ch2, // （CH2 数据接口，预留）
+
+    // DAC_ctrl（DAC控制）
+    output dac_ctrl_spi_cs,
+    output dac_ctrl_spi_sck,
+    output dac_ctrl_spi_mosi,
+    input dac_ctrl_spi_miso,
+
+    // AFE_ctrl（模拟前端控制，预留）
+    output reg[3:0] AFE_Ctrl_ch1,
+    output reg[3:0] AFE_Ctrl_ch2
+);
+
+endmodule
