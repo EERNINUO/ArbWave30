456847b62c56269e52add97c502c7c50970fc645
eerninuo <eerninuo88@gmail.com>
2026-09-15 23:20:48 +0800

    feat：添加校准表

--- firmware/MCU/Application/Middlewares/User/calibration/calibration.c
+++ firmware/MCU/Application/Middlewares/User/calibration/calibration.c
@@ -17,11 +17,11 @@ typedef struct {
 
 static CalTable_t cal_table[2] = {
     {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-        false 
+        {44900, 45182, 45017, 44915, 44740, 44617, 44664, 44595, 44714, 44976, 45506, 46084, 46965, 48222, 49170, 51311}, 
+        true 
     },
     {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
         false 
     }
 }; 
@@ -37,7 +37,7 @@ static int16_t apply_gain(int16_t ideal_mV, int32_t gain)
 
 int16_t calibration(uint8_t channel, uint64_t freq_hz, int16_t ideal_mV)
 {
-    CalTable_t *table = &cal_table[channel];
+    CalTable_t *table = &cal_table[channel - 1];
     
     if (!table->cal_valid) {
         return ideal_mV;  // 无校准数据，直接返回
@@ -62,7 +62,7 @@ int16_t calibration(uint8_t channel, uint64_t freq_hz, int16_t ideal_mV)
             int32_t g0 = table -> cal_gain_points[i];
             int32_t g1 = table -> cal_gain_points[i+1];
             
-            int32_t gain = g0 + (int32_t)((int64_t)(freq_hz - f0) * (g1 - g0) / (f1 - f0));
+            int32_t gain = g0 + ((int32_t)((int64_t)(freq_hz - f0) * (g1 - g0) / (f1 - f0)) >> 16);
             return apply_gain(ideal_mV, gain);
         }
     }
